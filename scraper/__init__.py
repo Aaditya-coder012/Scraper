47874b0a872a9293ba55710db716d5bd7352700d
Aadityacoder-012 <barnwalaaditya5@gmail.com>
2025-12-17 23:16:04 +0545

    my new project

--- scraper/__init__.py
+++
@@ -1,5 +0,0 @@
-from .email_crawler import crawl_site
-from .fetcher import HTMLFetcher
-from .parser import HTMLParser
-from .metadata_extractor import MetadataExtractor
-from .whois_lookup import WhoisLookup
--- scraper/parser.py
+++
@@ -1,6 +0,0 @@
-from bs4 import BeautifulSoup
-
-class HTMLParser:
-    def parse(self, html):
-        # Using built-in parser for better compatibility
-        return BeautifulSoup(html, "html.parser")
